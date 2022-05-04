1d1d8c6ec948543bee037c0a33379b5f38b00099
Ramath <ramathmanjitha@gmail.com>
2022-05-04 16:12:15 +0530

    initial commit

---
+++ index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import {render} from "react-dom";
+
+render(<h1>Hello to react</h1>, document.getElementById('app'));
+//import React from 'react';
+//import {render} from 'react-dom';
+//render(<h1>Hello to React</h1>, document.getElementById('app'));
